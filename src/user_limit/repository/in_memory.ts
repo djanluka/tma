c849e633348508fc9410ca98b71c9c75f446426e
Boris Cvitak <boris.cvitak@fincore.com>
2025-07-23 18:47:19 +0200

    refactor: in memory repository result type

--- src/user_limit/repository/in_memory.ts
+++ src/user_limit/repository/in_memory.ts
@@ -1,6 +1,6 @@
 import { CreatedEventPayload, ProgressChangedEventPayload, ResetEventPayload, UserLimit } from '../types.js';
 
-class Result {
+class RepositoryResult {
   constructor(
     public readonly success: boolean,
     public readonly error?: string
@@ -12,28 +12,28 @@ class Result {
   }
 
   // Static factory methods
-  static success(): Result {
-    return new Result(true);
+  static success(): RepositoryResult {
+    return new RepositoryResult(true);
   }
 
-  static fail(error: string): Result {
-    return new Result(false, error);
+  static fail(error: string): RepositoryResult {
+    return new RepositoryResult(false, error);
   }
 }
 
 
 // Repository
 export interface UserLimitRepository {
-  create(payload: CreatedEventPayload): Promise<Result>;
-  updateProgress(payload: ProgressChangedEventPayload): Promise<Result>;
-  resetProgress(payload: ResetEventPayload): Promise<Result>;
+  create(payload: CreatedEventPayload): Promise<RepositoryResult>;
+  updateProgress(payload: ProgressChangedEventPayload): Promise<RepositoryResult>;
+  resetProgress(payload: ResetEventPayload): Promise<RepositoryResult>;
   // findById(userLimitId: string): Promise<UserLimit | null>;
 }
 
 export class InMemoryUserLimitRepository implements UserLimitRepository {
   private limits: Map<string, UserLimit> = new Map();
 
-  async create(payload: CreatedEventPayload): Promise<Result> {
+  async create(payload: CreatedEventPayload): Promise<RepositoryResult> {
     const ul: UserLimit = {
       activeFrom: payload.activeFrom,
       brandId: payload.brandId,
@@ -50,13 +50,13 @@ export class InMemoryUserLimitRepository implements UserLimitRepository {
     };
     this.limits.set(ul.userLimitId, ul)
 
-    return Promise.resolve(Result.success())
+    return Promise.resolve(RepositoryResult.success())
   }
 
-  async updateProgress(payload: ProgressChangedEventPayload): Promise<Result> {
+  async updateProgress(payload: ProgressChangedEventPayload): Promise<RepositoryResult> {
     const ul = this.limits.get(payload.userLimitId);
     if (!ul) {
-      return Promise.resolve(Result.fail(`Not found: ${payload.userLimitId}`));
+      return Promise.resolve(RepositoryResult.fail(`Not found: ${payload.userLimitId}`));
     }
 
     // Check progress calculation
@@ -65,7 +65,7 @@ export class InMemoryUserLimitRepository implements UserLimitRepository {
     const amount = parseFloat(payload.amount);
     const remainingAmount = parseFloat(payload.remainingAmount);
     if (value !== previousProgress + amount + remainingAmount) {
-      return Promise.resolve(Result.fail(`Failed progress calculation for: ${payload.userLimitId}`));
+      return Promise.resolve(RepositoryResult.fail(`Failed progress calculation for: ${payload.userLimitId}`));
     }
 
     // Update progress and update UserLimit
@@ -73,13 +73,13 @@ export class InMemoryUserLimitRepository implements UserLimitRepository {
     ul.progress = (previousProgress + amount).toString();
     this.limits.set(ul.userLimitId, ul)
 
-    return Promise.resolve(Result.success())
+    return Promise.resolve(RepositoryResult.success())
   }
 
-  async resetProgress(payload: ResetEventPayload): Promise<Result> {
+  async resetProgress(payload: ResetEventPayload): Promise<RepositoryResult> {
     const ul = this.limits.get(payload.userLimitId);
     if (!ul) {
-      return Promise.resolve(Result.fail(`Not found: ${payload.userLimitId}`));
+      return Promise.resolve(RepositoryResult.fail(`Not found: ${payload.userLimitId}`));
     }
 
     // Reset progress and update UserLimit
@@ -88,7 +88,7 @@ export class InMemoryUserLimitRepository implements UserLimitRepository {
     ul.previousLimitValue = ul.value;
     this.limits.set(ul.userLimitId, ul)
 
-    return Promise.resolve(Result.success())
+    return Promise.resolve(RepositoryResult.success())
   }
 
   // async findById(userLimitId: string): Promise<UserLimit | null> {
